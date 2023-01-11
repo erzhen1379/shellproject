c6c52d4fd59a69629ae9bdd7d56c37c667a9d547
wuerzhen <1160115913@qq.com>
2023-01-11 11:18:31 +0800

    other

---
+++ chapter03/3.09filestat.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#文件名: filestat.sh
+
+if [ $# -ne 1 ];
+   then 
+        echo 'enter args failed'
+    exit
+fi
+path=$1
+
+declare -A statarray;
+while read line;
+do 
+ftype='file -b "$line" |cut -d, -f1' 
+let statarray["$ftype"]++;
+done < <(find $path -type f -print)
+echo ===============File type and counts ===============================
+for ftype in "${!statarray[@]}";
+do 
+echo $ftype : ${statarray["$ftype"]}
+done
